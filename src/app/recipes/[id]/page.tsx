eb4fdf17479da0a3ab80ba43ab614770ce93f573
Connor Littleton <littletonconnor@gmail.com>
2023-05-18 22:58:12 -0700

    feat: :fire: Start using contentlayer

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -1,50 +1,77 @@
+import { Mdx } from '@/lib/mdx';
+import { allRecipes } from 'contentlayer/generated';
+import { notFound } from 'next/navigation';
+
+export async function getStaticParams() {
+  return allRecipes.map((recipe) => {
+    return {
+      slug: recipe.slug,
+    };
+  });
+}
+
 export default async function Page({
+  params,
   searchParams,
 }: {
   params: { id: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const tableData = getTableData(Number(searchParams.offset) ?? 0);
+  console.log('allRecipe', allRecipes);
+  const recipe = allRecipes.find((recipe) => recipe.slug === params.id);
+
+  if (!recipe) {
+    return notFound();
+  }
 
   return (
-    <div className='flex flex-col space-y-4'>
-      <h2 className='text-2xl font-bold'>Pagination</h2>
-      <div className='relative overflow-x-auto border border-slate-200 shadow-md sm:rounded-lg'>
-        <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
-          <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
-            <tr>
-              <th scope='col' className='px-6 py-3'>
-                Id
-              </th>
-              <th scope='col' className='px-6 py-3'>
-                Name
-              </th>
-              <th scope='col' className='px-6 py-3'>
-                email
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableData.map((user) => (
-              <tr
-                key={user.id}
-                className='border-b bg-white dark:border-gray-700 dark:bg-gray-900'
-              >
-                <th
-                  scope='row'
-                  className='whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white'
-                >
-                  {user.id}
-                </th>
-                <td className='px-6 py-4'>{user.name}</td>
-                <td className='px-6 py-4'>{user.email}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <section className='flex flex-col space-y-4'>
+      <h2 className='text-2xl font-bold'>{recipe.title}</h2>
+      <time>{new Date(recipe.date).toLocaleDateString()}</time>
+      <Mdx code={recipe.body.code} />
+    </section>
   );
+
+  // return (
+  // <div className='flex flex-col space-y-4'>
+  //   <h2 className='text-2xl font-bold'>Pagination</h2>
+  //   <div className='relative overflow-x-auto border border-slate-200 shadow-md sm:rounded-lg'>
+  //     <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
+  //       <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
+  //         <tr>
+  //           <th scope='col' className='px-6 py-3'>
+  //             Id
+  //           </th>
+  //           <th scope='col' className='px-6 py-3'>
+  //             Name
+  //           </th>
+  //           <th scope='col' className='px-6 py-3'>
+  //             email
+  //           </th>
+  //         </tr>
+  //       </thead>
+  //       <tbody>
+  //         {tableData.map((user) => (
+  //           <tr
+  //             key={user.id}
+  //             className='border-b bg-white dark:border-gray-700 dark:bg-gray-900'
+  //           >
+  //             <th
+  //               scope='row'
+  //               className='whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white'
+  //             >
+  //               {user.id}
+  //             </th>
+  //             <td className='px-6 py-4'>{user.name}</td>
+  //             <td className='px-6 py-4'>{user.email}</td>
+  //           </tr>
+  //         ))}
+  //       </tbody>
+  //     </table>
+  //   </div>
+  // </div>
+  // );
 }
 
 function generateUsers(n: number) {
